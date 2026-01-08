35374e7d631de259441521ba984ee02a92b5c464
yusuf.erdem <yusuffamilyacc@gmail.com>
2026-01-08 14:57:53 +0300

    sdfsdf

--- lib/auth.ts
+++ lib/auth.ts
@@ -46,14 +46,14 @@ export const authOptions = {
     })
   ],
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, user }: { token: any; user: any }) {
       if (user) {
         token.role = user.role
         token.id = user.id
       }
       return token
     },
-    async session({ session, token }) {
+    async session({ session, token }: { session: any; token: any }) {
       if (session.user) {
         session.user.role = token.role as string
         session.user.id = token.id as string
